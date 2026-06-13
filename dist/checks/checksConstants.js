7228b7335fb5565f4142165350824d58cbe36e20
actionchain <actionchain@chainguard.dev>
2026-06-13 20:32:23 +0000

    Update wechuli/allcheckspassed@v2.2.0

---
+++ dist/checks/checksConstants.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GitHubActionsBotSlug = exports.checkStatus = exports.checkConclusion = void 0;
+exports.checkConclusion = {
+    ACTION_REQUIRED: "action_required",
+    CANCELLED: "cancelled",
+    FAILURE: "failure",
+    NEUTRAL: "neutral",
+    SUCCESS: "success",
+    SKIPPED: "skipped",
+    STALE: "stale",
+    TIMED_OUT: "timed_out",
+};
+exports.checkStatus = {
+    QUEUED: "queued",
+    IN_PROGRESS: "in_progress",
+    COMPLETED: "completed",
+    WAITING: "waiting",
+};
+exports.GitHubActionsBotSlug = "github-actions";
+//# sourceMappingURL=checksConstants.js.map
